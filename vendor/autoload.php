2c8a4edd16848adeb6c602d2c4f0473f8bff9f51
Herb Miller <herb@bobbingwide.com>
2020-04-08 09:00:47 +0100

    jetpack 8.4 - 2020-04-07

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitcc63bc401fb847c27bc52a1b6ff9376f::getLoader();
+return ComposerAutoloaderInit00a0b31576a1023081fd30b8697c570e::getLoader();
